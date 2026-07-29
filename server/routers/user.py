e2d2a6f5fa4abb96c43e4a342600d33f9fb01c5a
archie955 <archiesmedley@gmail.com>
2026-07-29 16:46:29 +0100

    first iteration of openid integration in logging in

--- server/routers/user.py
+++ server/routers/user.py
@@ -1,1 +1,33 @@
+# This entire router will be refactored into a service layer + router
 
+from fastapi import APIRouter, Request, status
+from fastapi.responses import RedirectResponse
+
+from exceptions.app_exceptions import InvalidCredentialsError
+from schemas import user_schemas
+from utils.steam_login import SteamLogin, SteamValidator
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+
+@router.post("", status_code=status.HTTP_303_SEE_OTHER, response_model=RedirectResponse)
+def login(request: Request):
+    url = f"{request.url.path}/validatelogin"
+    steam = SteamLogin(url)
+    return steam.Redirect()
+
+
+@router.get(
+    "/validatelogin", status_code=status.HTTP_200_OK, response_model=user_schemas.login
+)
+def validate_login(request: Request):
+    validator = SteamValidator()
+    steamID = validator.ValidateLogin(request.query_params)
+
+    if not steamID or not isinstance(steamID, str):
+        raise InvalidCredentialsError()
+
+    # commit to database when I know what details I actually want
+
+    # update schema when database models completed
+    return user_schemas.login(steam_id=steamID)
--- server/utils/steam_login.py
+++ server/utils/steam_login.py
@@ -1,4 +1,6 @@
+from urllib.parse import urlencode
 
+import requests
 from fastapi import status
 from fastapi.responses import RedirectResponse
 
@@ -15,7 +17,7 @@ openid.return_to=https%3A%2F%2Fcsstats.gg%2Flogin%2Ffc815c99369f80f3f4ff9d9a9498
 
 class SteamLogin:
     def __init__(self, home_url: str):
-        self.__baseurl = "https://steamcommunity.com/openid/login?"
+        self.__baseurl = "https://steamcommunity.com/openid/login"
         self.__params = {
             "openid.claimed_id": "https://specs.openid.net/auth/2.0/identifier_select",
             "openid.identity": "http://specs.openid.net/auth/2.0/identifier_select",
@@ -24,12 +26,13 @@ class SteamLogin:
             "openid.realm": home_url,
             "openid.return_to": home_url,
         }
-        self.__validationParams = {}
 
     def __createUrl(self) -> str:
-        return "%s?%s"
+        return f"{self.__baseurl}?{urlencode(self.__params)}"
 
-    def __redirect(self) -> RedirectResponse:
+    # This redirects to steam.
+    # Upon login it will send a request to the return_to url provided.
+    def Redirect(self) -> RedirectResponse:
         url = self.__createUrl()
         response = RedirectResponse(
             url=url,
@@ -38,8 +41,53 @@ class SteamLogin:
         )
         return response
 
-    def __validateLogin(self, data):
-        pass
 
-    def login(self):
-        pass
+class SteamValidator:
+    def __init__(self):
+        self.__baseurl = "https://steamcommunity.com/openid/login"
+        self.__validationParams = {}
+
+    def ValidateLogin(self, data) -> str | bool:
+        string_params = (
+            "openid.ns",
+            "openid.mode",
+            "openid.op_endpoint",
+            "openid.claimed_id",
+            "openid.identity",
+            "openid.return_to",
+            "openid.response_nonce",
+            "openid.assoc_handle",
+            "openid.signed",
+            "openid.sig",
+        )
+
+        for param in string_params:
+            val = data[param]
+            if not val or not isinstance(val, str):
+                return False
+            self.__validationParams[param] = val
+
+        self.__validationParams["openid.mode"] = "check_authentication"
+
+        response = requests.get(self.__baseurl, params=self.__validationParams).text
+
+        validator = {}
+
+        for line in response.splitlines():
+            if ":" in line:
+                k, v = line.split(":", 1)
+                validator[k.strip()] = v.strip()
+
+        if validator["isvalid"] != "true":
+            return False
+
+        identity = data["openid.identity"]
+        if identity != data["openid.claimed_id"]:
+            return False
+
+        prefix = "https://steamcommunity.com/openid/id/"
+        p = len(prefix)
+        if identity[:p] != prefix:
+            return False
+
+        return identity[p:]
